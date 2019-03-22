b1332cee733382f7b943e677d30dac97002a3bdc
david4neblio <43017430+david4neblio@users.noreply.github.com>
2019-03-22 09:32:18 -0400

    Added build script

---
+++ bin/build_native.sh
@@ -0,0 +1,1 @@
+mkbundle -o neblidex_blockhelper --simple BlockHelper.exe -L ./ -L /Library/Frameworks/Mono.framework/Versions/Current/lib/mono/4.5/ --machine-config /Library/Frameworks/Mono.framework/Versions/Current/etc/mono/4.5/machine.config --config /Library/Frameworks/Mono.framework/Versions/Current/etc/mono/config --runtime /Library/Frameworks/Mono.framework/Versions/Current/Commands/mono
